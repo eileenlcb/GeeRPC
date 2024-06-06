f03702a257e363460535c0a1968eedf431ba855f
Liu Can <524840526@qq.com>
2024-06-06 19:13:17 +0800

    update

---
+++ main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"geerpc"
+	"geerpc/codec"
+	"log"
+	"net"
+	"time"
+)
+
+func startServer(addr chan string) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	geerpc.Accept(l)
+}
+
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	conn, _ := net.Dial("tcp", <-addr)
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	time.Sleep(time.Second)
+
+	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+}
