b7a11fc988bc6d321eecce3c2329db6d02a81888
Liu Can <524840526@qq.com>
2024-06-06 15:35:06 +0800

    day1

--- server.go
+++ server.go
@@ -2,10 +2,12 @@ package geerpc
 
 import (
 	"encoding/json"
+	"fmt"
 	"geerpc/codec"
 	"io"
 	"log"
 	"net"
+	"reflect"
 	"sync"
 )
 
@@ -71,5 +73,61 @@ func (server *Server) serveCodec(cc codec.Codec) {
 	for {
 		req, err := server.readRequest(cc)
 		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
 	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+type request struct {
+	h            *codec.Header
+	argv, replyv reflect.Value
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read body error:", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
+
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
