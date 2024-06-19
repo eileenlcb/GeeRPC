7055e9e6f4f7ead92136fe56e3a51e324c883efb
Liu Can <524840526@qq.com>
2024-06-19 17:38:19 +0800

    update

--- service.go
+++ service.go
@@ -1,6 +1,8 @@
 package geerpc
 
 import (
+	"go/ast"
+	"log"
 	"reflect"
 	"sync/atomic"
 )
@@ -12,6 +14,13 @@ type methodType struct {
 	numCalls  uint64
 }
 
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
 func (m *methodType) NumCalls() uint64 {
 	//原子操作加载unint64类型的值
 	return atomic.LoadUint64(&m.numCalls)
@@ -20,8 +29,6 @@ func (m *methodType) NumCalls() uint64 {
 // reflect相关解释：
 // reflect.New: 返回一个Value类型，该类型包含一个指向类型为typ的新申请的零值
 // reflect.Value:反射包中一个代表Go值的通用容器，可以操作任意类型的值
-
-
 func (m *methodType) newArgv() reflect.Value {
 	var argv reflect.Value
 	//两种argv类型：值类型和指针类型，example输出：
@@ -48,4 +55,57 @@ func (m *methodType) newReplyv() reflect.Value {
 		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
 	}
 	return replyv
+}
+
+func newService(rcvr interface{}) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if ast.IsExported(s.name) {
+		log.Fatal("rpc server: ", s.name, " is not a valid service name")
+	}
+	s.registerMethods()
+	return s
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumField(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		//作为rpc调用，包含自身在内，numIn必须为3（自身/导出的方法/返回值指针）;numOut必须为1（error返回值）
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		//(*error)(nil)用于得到一个值为nil的error指针类型;Elem()会直接返回'error'这个类型
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc server: register %s.%s\n", s.name, method.Name)
+	}
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	//returnValues是一个Value类型的切片
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
 }
---
+++ test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sync/atomic"
+)
+
+type service struct {
+	rcvr reflect.Value
+}
+
+type methodType struct {
+	method   reflect.Method
+	numCalls uint64
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+// 一个测试方法
+func (s *service) ExampleMethod(arg int, reply *int) error {
+	*reply = arg + 1
+	return nil
+}
+
+func main() {
+	srv := &service{}
+	srv.rcvr = reflect.ValueOf(srv)
+
+	exampleMethod, _ := reflect.TypeOf(srv).MethodByName("ExampleMethod")
+	m := &methodType{method: exampleMethod}
+
+	argv := reflect.ValueOf(42)
+	replyv := reflect.New(reflect.TypeOf(0)).Elem()
+
+	err := srv.call(m, argv, replyv)
+	fmt.Println("Error:", err)
+	fmt.Println("Reply:", replyv.Int())
+}
