739dfa0febb5207951115a6bb9ed3f8d7e9074a9
eileenlcb <524840526@qq.com>
2024-07-22 22:24:36 +0800

    update

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package geerpc
 
 import (
 	"net"
+	"strings"
 	"testing"
 	"time"
 )
@@ -26,5 +27,18 @@ func TestClient_dialTimeout(t *testing.T) {
 	t.Parallel()
 	l, _ := net.Listen("tcp", ":0")
 	// addr := make(chan string)
-	f:=func(con net.Conn,opt *Option)(client *Client,err error){
+	f := func(con net.Conn, opt *Option) (client *Client, err error) {
+		_ = con.Close()
+		time.Sleep(2 * time.Second)
+		return nil, nil
+	}
+	t.Run("timeout", func(t *testing.T) {
+		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: time.Second})
+		_assert(err != nil && strings.Contains(err.Error(), "connect timeout"), "expect a timeout error")
+	})
+
+	t.Run("0", func(t *testing.T) {
+		_, err := dialTimeout(f, "tcp", l.Addr().String(), &Option{ConnectTimeout: 0})
+		_assert(err == nil, "0 means no limit")
+	})
 }
